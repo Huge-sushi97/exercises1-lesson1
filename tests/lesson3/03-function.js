8c290afd27d0ee2b5e740dbd607fbb4bc7e7a272
Phu Le <levinhphu201297@gmail.com>
2025-06-21 14:12:37 +0700

    add question 3,4 for functions

--- tests/lesson3/03-function.js
+++ tests/lesson3/03-function.js
@@ -1,17 +1,38 @@
 //Cau1
-function multiply (a,b){
-    console.log("Ket qua sau khi nhan a va b la: ", a*b);
+function multiply(a, b) {
+    console.log("Ket qua sau khi nhan a va b la: ", a * b);
 }
-multiply(2,3);
-multiply(10,10);
+multiply(2, 3);
+multiply(10, 10);
 
 //Cau2
-function findMin (a,b,c){
-    let min = Math.min(a,b,c);
+function findMin(a, b, c) {
+    let min = Math.min(a, b, c);
     console.log("Gia tri nho nhat la: ", min);
 }
-findMin(2,3,4);
-findMin(10,11,15);
-findMin(10,100,199);
+findMin(2, 3, 4);
+findMin(10, 11, 15);
+findMin(10, 100, 199);
 
-//Cau3,4 -> cho no lai lan sau nha hihi <^_^>
+//Cau3
+function getTopStudents(arr1, threshold) {
+    for (let item of arr1) {
+        if (item.score >= threshold) {
+            console.log(item.name);
+        }
+
+    }
+}
+getTopStudents(
+    [
+        { name: "Alice", score: 8 },
+        { name: "Bob", score: 9 },
+        { name: "Charlie", score: 10 }
+    ], 9)
+
+//Cau4 
+function calculateInterest(_principal, _rate, _years) {
+    let total = (_principal + _principal * _rate * _years) / 100;
+    console.log(total);
+}
+calculateInterest(12000, 4, 2);
